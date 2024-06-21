62c623ba196965622da8116f8dc3a52d6194edfc
online <154216964+online3312@users.noreply.github.com>
2024-06-21 23:33:26 +0900

    Add files via upload

---
+++ Message Box dll/Message Box/dllmain.cpp
@@ -0,0 +1,17 @@
+﻿#include <windows.h>
+#include <pch.h>
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, L"DLL attached", L"Info", MB_OK | MB_ICONINFORMATION); // L prefix for wide string
+        break;
+    case DLL_PROCESS_DETACH:
+        MessageBox(NULL, L"DLL detached", L"Info", MB_OK | MB_ICONINFORMATION); // L prefix for wide string
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ Message box exe/Message box exe/Message box exe.cpp
@@ -0,0 +1,8 @@
+﻿// Message box exe.cpp
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL, L"Hello from MessageBoxApp.exe!", L"Message Box Example", MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
